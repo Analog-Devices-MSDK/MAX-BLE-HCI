2a41f0578702e4d8d175ede5281282b6d768fa8c
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-07-30 11:20:57 -0500

    added context manager and improved retry logic

--- src/max_ble_hci/ble_hci.py
+++ src/max_ble_hci/ble_hci.py
@@ -153,11 +153,11 @@ class BleHci(BleStandardCmds, VendorSpecificCmds):
         super().__init__(self.port, logger_name)
 
     def __enter__(self):
+        self.port.start()
         return self
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.reset()
-        pass
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.port.close()
 
     def get_log_level(self) -> str:
         """Retrieve the current log level.
